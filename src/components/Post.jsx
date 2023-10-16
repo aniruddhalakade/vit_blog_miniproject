cd4b9e15479fae6a3d88144210d700a7f6ea8cda
Eshwari <eshwarisawant@gmail.comgit config --global user.name Eshwarigit config --global user.email eshwarisawant@gmail.com>
2023-10-16 19:17:40 +0530

    sending to Aniruddha

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,9 +5,23 @@ function Post(){
         <div className="posts">
         
         <img className="immage" src="https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1nfGVufDB8fDB8fHww&w=1000&q=80" alt="img" />
-        <h2>first post</h2>
-        <p>dsjfkhdkjndknjdvjncjkdsnvdjfbjvndvjskjfbsjfnskjfnfkjsnfjskfbkbsvckjdskfnsfbchjbddfbfnjkfnskjfnsjkfjfnkncskjs</p>
+       <div className="Contentt">
+         <h1 className="heading">1.VIT council</h1>
+        <h2 className="tags">#student council, #sports, #vemps</h2>
+
+        <h2><p>INFT-A vs EXTC-B, VEMPS: inft won the match by 3 wicket, knocking out extc-b out of the tournament.
+        </p>
+        <br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam vulputate dignissim suspendisse in est ante in nibh mauris. Magna eget est lorem ipsum dolor. Dui accumsan sit amet nulla. Porttitor lacus luctus accumsan tortor posuere ac ut consequat. In nisl nisi scelerisque eu. Quam id leo in vitae turpis massa sed.</h2>
         </div>
+        </div>
+
+       
+
+       
+
+    
+
+       
     );
 }
 
